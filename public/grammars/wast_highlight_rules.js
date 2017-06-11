7d4972e28426769075ca6fefe44de9e454984da6
maxgraey <maxgraey@gmail.com>
2017-06-11 14:48:04 +0300

    update wast rules (WIP)

--- public/grammars/wast_highlight_rules.js
+++ public/grammars/wast_highlight_rules.js
@@ -5,8 +5,8 @@ ace.define('ace/mode/wast_highlight_rules', function (acequire, exports, module)
     var TextHighlightRules = acequire("ace/mode/text_highlight_rules").TextHighlightRules;
 
     var WastHighlightRules = function () {
-        var keywords = "global|local|type|func|memory|module|import|export|param|result";
-        var types    = "i32|i64|f32|f64|none";
+        var keywords = "global|local|type|memory|module|table|import|export|param|offset|start|code|data|element";
+        var types    = "i32|i64|f32|f64|anyfunc|func|void";
         var operator = [
             "const|if|else|loop|block|br|br_if|br_table|drop|select|end|return|call|call_indirect",
             "current_memory|grow_memory|get_local|set_local|tee_local|get_global|set_global",
@@ -18,30 +18,30 @@ ace.define('ace/mode/wast_highlight_rules', function (acequire, exports, module)
         ].join("|");
 
         var keywordMapper = this.createKeywordMapper({
-            "keyword.control":   keywords,
-            "keyword.operator":  operator,
-            "support.function":  types
+            "keyword.control":  keywords,
+            "keyword.operator": operator,
+            "support.function": types
         }, "identifier", true);
 
         this.$rules = {
             "start": [{
-                token : "constant.numeric", // hex
-                regex : /0(?:[xX][0-9a-fA-F][0-9a-fA-F_]*|[bB][01][01_]*)\b/
+                token: "constant.numeric", // hex
+                regex: /0(?:[xX][0-9a-fA-F][0-9a-fA-F_]*|[bB][01][01_]*)\b/
             }, {
-                token : "constant.numeric", // float
-                regex : /[+-]?\d[\d_]*(?:(?:\.[\d_]*)?(?:[eE][+-]?[\d_]+)?)\b/
+                token: "constant.numeric", // float
+                regex: /[+-]?\d[\d_]*(?:(?:\.[\d_]*)?(?:[eE][+-]?[\d_]+)?)\b/
             }, {
-                token : keywordMapper,
-                regex : "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
+                token: keywordMapper,
+                regex: "[a-zA-Z_$][a-zA-Z0-9_$]*\\b"
             }, {
-                token : "lparen",
-                regex : "[[({]"
+                token: "lparen",
+                regex: "[[({]"
             }, {
-                token : "rparen",
-                regex : "[\\])}]"
+                token: "rparen",
+                regex: "[\\])}]"
             }, {
-                token : "text",
-                regex : "\\s+"
+                token: "text",
+                regex: "\\s+"
             }]
         };
     };
