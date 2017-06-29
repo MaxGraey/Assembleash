3eff0442c7b2ad31c667c5108d17b73b3cbefc4e
maxgraey <maxgraey@gmail.com>
2017-06-29 19:56:02 +0300

    improve footer layout

--- src/Containers/FooterContainer.js
+++ src/Containers/FooterContainer.js
@@ -58,30 +58,32 @@ export default class FooterContainer extends Component {
         return (
             <ButtonToolbar className="navbar-fixed-bottom" style={{ textAlign: 'center', padding: 0, margin: '20px 20px 7px 15px' }}>
                 <Button bsSize='large' bsStyle='info' className="pull-right" disabled={ downloadDisabled } onClick={ onDownloadPressed }>
-                    <span><Glyphicon glyph="download" style={{ fontSize: "125%", marginTop: '-0.5rem', top: '0.5rem' }}/>Download .wasm</span>
+                    <span><Glyphicon glyph="download" style={{ fontSize: "125%", marginTop: '-0.25em', top: '0.25em' }}/>Download .wasm</span>
                 </Button>
-                <div className="pull-right label">
+                <div className="pull-right label" style={{ paddingRight: '2em', paddingTop: '3pt' }}>
                     <h4>{ size }
-                        <span style={{ color: '#bbb', paddingRight: '1em', fontWeight: 100 }}>{ ' ' + unit }</span>
+                        <span style={{ color: '#bbb', fontWeight: 100 }}>{ ' ' + unit }</span>
                     </h4>
                 </div>
-                <label className="pull-left" style={{ paddingTop: '2pt', paddingLeft: '10pt' }}>
-                    <h4 style={{ fontWeight: 100, color: '#bbb' }}>{ cursorPosition[0] + ':' + cursorPosition[1] }</h4>
+                <label className="pull-left" style={{ marginTop: '3pt', marginLeft: '-2pt', minWidth: '51pt' }}>
+                    <h4 style={{ fontSize: '1.8rem', fontWeight: 100, color: '#aaa' }}>{ cursorPosition[0] + ':' + cursorPosition[1] }</h4>
                 </label>
-                <BusySignal className="pull-left" state={ busyState }/>
-                <label style={{
-                    marginLeft: '30pt',
-                    float:      'left',
-                    paddingTop: '3pt'
-                }}>
-                    <h4
-                        className={ messageClass }
-                        style={{
-                            fontWeight: 100,
-                            textShadow: '0 0 1px rgba(0,0,0,0.6)'
-                        }}
-                    >{ statusBarMessage }</h4>
-                </label>
+                <div className="pull-left" style={{ marginLeft: '-12pt' }}>
+                    <BusySignal className="pull-left" state={ busyState }/>
+                    <label style={{
+                        marginLeft: '30pt',
+                        float:      'left',
+                        paddingTop: '3pt'
+                    }}>
+                        <h4
+                            className={ messageClass }
+                            style={{
+                                fontWeight: 100,
+                                textShadow: '0 0 1px rgba(0,0,0,0.6)'
+                            }}
+                        >{ statusBarMessage }</h4>
+                    </label>
+                </div>
             </ButtonToolbar>
         );
     }
